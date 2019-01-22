909f561aa0b299916172f87a27ca8f85d68072c5
Lab4 <Lab4@PC-PBKGKVR>
2019-01-21 17:12:19 -0700

    update

--- test2.py
+++ test2.py
@@ -1,8 +1,9 @@
 print ('hello')
+import time
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 #from pymodbus.registger_read_message import ReadInputRegisterResponse
 
-client = ModbusClient(method = 'rtu' , port = 'COM10' , stopbits=1, parity ='N', baudrate='115200' ,timeout=0.5)#, unit='0x01')
+client = ModbusClient(method = 'rtu' , port = 'COM9' , stopbits=1, parity ='N', baudrate='115200' ,timeout=0.5)#, unit='0x01')
 #client = ModbusClient(method = 'rtu' , port = 'COM10' , stopbits=1, bytesize=8, parity ='N', baudrate='115200' ,timeout=0.5)
 #client = ModbusClient(method = 'rtu' , port = 'COM10' , stopbits=1, bytesize=8, parity ='N', baudrate='9600' ,timeout=0.3)
 
@@ -14,7 +15,12 @@ client = ModbusClient(method = 'rtu' , port = 'COM10' , stopbits=1, parity ='N',
 connection = client.connect()
 print(connection)
 
-response = client.read_holding_registers(5300,110,unit=1)
+client.write_register(5054, 17530)
+client.write_register(5055, 0)
+
+time.sleep(3)
+
+response = client.read_holding_registers(5047,12,unit=1)
 print response.registers
 print response.registers[0]
 print ('xXx')
